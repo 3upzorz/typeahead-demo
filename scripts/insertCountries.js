3e833516f39968c8fb1d4ac17fe8ba82cb90da5a
Trevor Hall <trevor.hall@viseo.com>
2017-05-29 03:03:19 +0800

    change insert to bulk

--- scripts/insertCountries.js
+++ scripts/insertCountries.js
@@ -3,38 +3,24 @@ const { elasticsearchConfig } = require('../config/config');
 const countries = require('../data/countries.json');
 
 const client = new elasticsearch.Client(elasticsearchConfig);
-// elasticsearch only supports up to 200 create actions per batch by default
-const elasticsearchBatchMax = 200;
 
-const firstBatch = countries.slice(0, elasticsearchBatchMax).map((country, arrayIndex) => {
-  return client.create({
-    index: 'countries',
-    type: 'country',
-    timeout: '5m',
-    id: arrayIndex,
-    body: {
-      name: country,
-      name_suggest: country.replace(/[\W_]+/g,'')
-    }
-  });
-});
-
-Promise.all(firstBatch)
-       .then((res) => {
-         const secondBatch = countries.slice(elasticsearchBatchMax).map((country, arrayIndex) => {
-           return client.create({
-             index: 'countries',
-             type: 'country',
-             timeout: '5m',
-             id: arrayIndex + 200,
-             body: {
-               name: country,
-               name_suggest: country.replace(/[\W_]+/g,'')
-             }
-           });
-         });
-         Promise.all(secondBatch)
-                .then(console.log)
-                .catch(console.error)
-       })
-       .catch(console.error)
+client.bulk({
+  body: countries.reduce((commands, country, arrayIndex) => {
+    commands.push(
+      {
+        index: {
+          _index: 'countries',
+          _type: 'country',
+          _id: arrayIndex
+        }
+      },
+      {
+        name: country,
+        name_suggest: country.replace(/[\W_]+/g,'')
+      }
+    );
+    return commands;
+  }, [])
+})
+.then(console.log)
+.catch(console.error);
